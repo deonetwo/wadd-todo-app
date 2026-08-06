4a516d85e11156a6f6a3006d75d71139dba46ff8
Dewanto Joyo Pramono <dewantoade11@gmail.com>
2026-08-06 08:58:38 +0700

    fix: resolve build errors and upgrade Avalonia

--- src/Wadd.Android/MainActivity.cs
+++ src/Wadd.Android/MainActivity.cs
@@ -1,22 +1,40 @@
+using System;
 using Android.App;
 using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
 using Avalonia;
 using Avalonia.Android;
 using Wadd.UI;
 
 namespace Wadd.Android;
 
-[Activity(
-    Label = "Wadd",
-    Theme = "@style/MyTheme.NoActionBar",
-    Icon = "@drawable/icon",
-    MainLauncher = true,
-    ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
-public class MainActivity : AvaloniaMainActivity<App>
+[Application]
+public class AndroidApplication : AvaloniaAndroidApplication<App>
 {
+    public AndroidApplication(IntPtr handle, JniHandleOwnership transfer)
+        : base(handle, transfer)
+    {
+    }
+
     protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
     {
         return base.CustomizeAppBuilder(builder)
             .WithInterFont();
+    }
+}
+
+[Activity(
+    Label = "Wadd",
+    Theme = "@style/MainTheme",
+    Icon = "@drawable/icon",
+    MainLauncher = true,
+    ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
+public class MainActivity : AvaloniaMainActivity
+{
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        SQLitePCL.Batteries_V2.Init();
+        base.OnCreate(savedInstanceState);
     }
 }
